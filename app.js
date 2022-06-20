c8166c86b5cd9b12bade78e810da23d0686c59a4
Amarachi Ajah     -     Software Developer | UX/UI Designer <anitaisbeta@gmail.com>
2022-06-20 20:49:10 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var person = {
+    name: 'David',
+    age: 15,
+    country: 'Programmer'
+  };
+  
+  const { name, age, country } = person; // Takes the property/method from the object
+  
+  console.log(name); // Prints 'David'
+  console.log(age); // Prints '15'
